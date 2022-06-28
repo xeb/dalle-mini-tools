d7bec8d9a1ac5b87fb9bcfe5ae6033e5af312139
Matt Mollison <matt.mollison@gmail.com>
2022-06-28 00:29:22 -0400

    fancier number text

--- montage.sh
+++ montage.sh
@@ -5,7 +5,16 @@ HEIGHT=128
 convert -size 1024x$HEIGHT -background white -bordercolor white -pointsize 32 -fill black caption:"$(cat prompt.txt)" -flatten -border 10x15 title.png
 number=1
 for f in img_*.png; do
-    convert "$f" -gravity southwest -background gray90 label:"$number" -composite miff:-
+    convert \( -size 18x18 xc:white \) \
+    \( +clone  -alpha extract \
+    -draw 'fill black polygon 0,0 0,15 15,0 fill white circle 1,1 15,0' \
+    \( +clone -flip \) -compose Multiply -composite \
+    \( +clone -flop \) -compose Multiply -composite \
+    \) -alpha off -compose CopyOpacity -composite \
+    \( -size 18x18 -background none -fill white -gravity center \
+    -pointsize 18 -family arial -weight Heavy label:"$number" \) \
+    -gravity southwest -compose dstout -composite -alpha on \
+    "$f" +swap -compose over -composite miff:-
     ((number++))
 done | montage -geometry +2+2 -bordercolor white -border 10x0 - all.png
 montage title.png all.png -geometry +1+1 -gravity south -tile 1x final.png
